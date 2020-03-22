41c2d449277cf72aad7607d75a4aee3ad504a344
Sagar Bhure <sagarbhureaerospace@gmail.com>
2020-03-22 07:20:14 +0530

    Update install-opencv.sh

--- install-opencv.sh
+++ install-opencv.sh
@@ -17,6 +17,7 @@ sudo apt-get install -y qt5-default libvtk6-dev
 # Media I/O:
 sudo apt-get install -y zlib1g-dev libjpeg-dev libwebp-dev libpng-dev libtiff5-dev libjasper-dev libopenexr-dev libgdal-dev
 
+
 # Video I/O:
 sudo apt-get install -y libdc1394-22-dev libavcodec-dev libavformat-dev libswscale-dev libtheora-dev libvorbis-dev libxvidcore-dev libx264-dev yasm libopencore-amrnb-dev libopencore-amrwb-dev libv4l-dev libxine2-dev
 
